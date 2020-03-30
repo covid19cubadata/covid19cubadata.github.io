0bfb24e93e7ef4e1af206be066f35b2a9084d240
Yudivian Almeida <yudivian@gmail.com>
2020-03-29 21:53:48 -0400

    Adding new charts (mainly tests and plit nations chart)

--- js/custom.js
+++ js/custom.js
@@ -59,6 +59,10 @@ $.getJSON("data/municipios.geojson",
 				'61 o más': 0,
 				'Desconocido': 0	
 			}
+			var total_cu = 0;
+			var total_no_cu = 0;
+			var total_unk = 0;
+			var total_tests = 0;
 			
 			for(var day in data.casos.dias){
 				if ('diagnosticados' in data.casos.dias[day]){
@@ -66,6 +70,17 @@ $.getJSON("data/municipios.geojson",
 					for(var p in diag){
 						cases[diag[p].id] = diag[p];
 						cases[diag[p].id]['fecha']= data.casos.dias[day].fecha;
+						
+						//cuban/no cuban
+						if (diag[p].pais=='cu'){
+							total_cu +=1; 
+						} else {
+							if (diag[p].pais=='unknown') {
+								total_unk +=1;
+							} else {
+								total_no_cu +=1;	
+							}
+						}
 						
 						//sex
 						if (diag[p].sexo=='hombre'){
@@ -116,6 +131,12 @@ $.getJSON("data/municipios.geojson",
 				if ('recuperados_numero' in data.casos.dias[day]){
 					recov += data.casos.dias[day].recuperados_numero;
 				}
+				
+				if ('tests_total' in data.casos.dias[day]){
+					if (total_tests<=data.casos.dias[day].tests_total){
+						total_tests=data.casos.dias[day].tests_total;
+					}
+				}
 			}
 			 
 			//Pie for sex
@@ -133,13 +154,44 @@ $.getJSON("data/municipios.geojson",
 			});
 			
 			
+			//Pie for cubans/no cubans
+			c3.generate({
+				bindto: "#countries-info-pie",
+				data: {
+				  columns: [['cubanos',total_cu],['extranjeros',total_no_cu],['no reportado',total_unk]] ,
+				  type: 'pie',
+				  colors: {
+					 'cubanos': '#B01E22',
+					 'extranjeros': '#1C1340',
+					 'no reportado': '#1A8323'
+				  }
+				}
+			});
+			
+			//Donut for tests
+			c3.generate({
+				bindto: "#tests-donut-info",
+				data: {
+				  columns: [['Casos Positivos',total_cu+total_no_cu+total_unk],['Casos Negativos',total_tests -(total_cu+total_no_cu+total_unk)]] ,
+				  type: 'donut',
+				  colors: {
+					 'Casos Positivos': '#B01E22',
+					 'Casos Negativos': '#1C1340',
+				  }
+				},
+		        donut: {
+		          title: total_tests+" tests",
+		        }
+			});
 			
 			//Bar for countries
 			var country = ['País'];
 			var countryDiagnoses = ['Diagnosticados'];
 			for(var c in countries){
-				country.push(getCountryFromDomain(c));
-				countryDiagnoses.push(countries[c]);	
+				if (c!='cu'){
+					country.push(getCountryFromDomain(c));
+					countryDiagnoses.push(countries[c]);	
+				}
 			}
 			c3.generate({
             bindto: "#countries-info",
@@ -219,6 +271,8 @@ $.getJSON("data/municipios.geojson",
 			var dailySingle = ['Casos en el día'];
 			var dailySum = ['Casos acumulados'];
 			var cuba = ['Cuba'];
+			var test_days = ['Fecha'];
+			var test_amount = ['Tests acumulados']
 			var total = 0;
 			
 			
@@ -232,10 +286,41 @@ $.getJSON("data/municipios.geojson",
 				} else {
 					dailySingle.push(0);
 				}
+				if ('tests_total' in data.casos.dias[i]){
+					test_days.push(data.casos.dias[i].fecha.replace('2020/',''));
+					test_amount.push(data.casos.dias[i].tests_total);	
+				}
 				dailySum.push(total);
 				cuba.push(total);
 			}
 			$('#update').html('2020/'+dates[dates.length-1]);
+			
+			
+			tests = c3.generate({
+					bindto: "#tests-line-info",
+						data: {
+							  x : test_days[0],
+							  columns: [
+								test_days,
+								test_amount
+							  ],
+							  type: 'bar',
+				              colors: {
+								 'Tests acumulados': '#B01E22' 
+							  }
+							},
+							axis: {
+							  x: {
+								label: 'Fecha',
+								type: 'categorical',
+								//show: false
+							  },
+							  y: {
+								label: 'Tests acumulados',
+								position: 'outer-middle'
+							  }
+							}
+					});
 			
 			var countrysorted= [];
 			for(var c in countriesdays.paises){
