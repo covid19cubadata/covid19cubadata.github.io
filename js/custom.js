01e5578c5121c3e8e4eae324017bbaba09b22e11
Yudivián Almeida Cruz <yudivian@gmail.com>
2020-04-15 17:12:55 -0400

    Hiding % tests graph form provinces view

--- js/custom.js
+++ js/custom.js
@@ -272,7 +272,7 @@ $.walker = {
             $('[data-content=recupe]').html('<i class="fa fa-spinner fa-spin"></i>');
 
             const general_view = $locator.val() === 'cuba';
-            let $generals = $('#recdist, #deadist, #tesmade-pcr, #tesacum, #topprov, #compari, #topn-n-countries, #evomade, #proscurves');
+            let $generals = $('#recdist, #deadist, #tesmade-pcr, #tesacum, #topprov, #compari, #topn-n-countries, #evomade, #proscurves, #testspor');
             if (general_view) {
                 $('#munscurves').css({'margin-left': ''});
                 $generals.show();
