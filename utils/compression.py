b27730df59d1bd4bbd741ddd9fd5573e89986926
hiancdtrsnm <hiancdtrsnm@gmail.com>
2021-02-04 00:13:32 -0500

    Fix CSV generation and generate ZIP with the data

---
+++ utils/compression.py
@@ -0,0 +1,10 @@
+import zipfile
+
+
+def compress():
+    zip = zipfile.ZipFile('data/covid19cuba.zip', 'w')
+    zip.write('data/covid19-cuba.json', compress_type=zipfile.ZIP_DEFLATED)
+    zip.write('data/covid19-fallecidos.json', compress_type=zipfile.ZIP_DEFLATED)
+    zip.write('data/covid19-casos.csv', compress_type=zipfile.ZIP_DEFLATED)
+
+    zip.close()
